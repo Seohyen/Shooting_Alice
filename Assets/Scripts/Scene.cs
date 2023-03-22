48d7cfc7e79a8bf5f5b5198e67150182e443fb9a
SeoHyun <namja4097@gmail.com>
2023-03-22 19:18:33 +0900

    나가기 버튼

--- Assets/Scripts/Scene.cs
+++ Assets/Scripts/Scene.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class Scene : MonoBehaviour
 {
-
+    [SerializeField]
+    private Button exitButton;
     public void SceneChange()
     {
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void Exit()
+    {
+        if (exitButton != null)
+        {
+            Application.Quit();
+        }
     }
 }
